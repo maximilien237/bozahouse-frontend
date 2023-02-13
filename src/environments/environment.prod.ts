a4e06919dc036f5ff23d75c6e85867fb2692bc1e
cooper-saxe <maximiliendenver@gmail.com>
2023-02-13 10:30:16 +0800

    new commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  backendHostPublic: "http://bozahouse.com:8085/api/auth/v1/",
-  backendHostEditor: "http://bozahouse.com:8085/api/editor/v1/",
-  backendHostAppUser: "http://bozahouse.com:8085/api/user/v1/",
-  backendHostAdmin: "http://bozahouse.com:8085/api/admin/v1/"
+  backendHostPublic: "https://www.bozahouse.com:8085/api/auth/v1/",
+  backendHostEditor: "https://www.bozahouse.com:8085/api/editor/v1/",
+  backendHostAppUser:"https://www.bozahouse.com:8085/api/user/v1/",
+  backendHostAdmin:  "https://www.bozahouse.com:8085/api/admin/v1/"
 };
