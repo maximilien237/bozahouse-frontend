626503fbf17a75732a366f0c4b7a427b0628ddd5
maximilien.kengne <maximiliendenver@gmail.com>
2024-02-01 11:16:55 +0100

    update angular to v15

--- src/app/services/guards/authentication.guard.ts
+++ src/app/services/guards/authentication.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import {AuthenticationService} from "../authentication/authentication.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthenticationGuard implements CanActivate {
+export class AuthenticationGuard  {
   constructor(private authenticationService: AuthenticationService, private router: Router) {
   }
 
